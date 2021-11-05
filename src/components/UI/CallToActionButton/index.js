910d101169270934ad68e3a1362e2a231a0625be
netdiatom <53354322+netdiatom@users.noreply.github.com>
2021-11-05 13:59:50 -0500

    Update index.js
    Changed hero text for cta button

--- src/components/UI/CallToActionButton/index.js
+++ src/components/UI/CallToActionButton/index.js
@@ -2,7 +2,8 @@ import './CallToActionButton.scss';
 export const CallToActionButton = () => (
   <div className="call-to-action-button">
     <div className="cta-text">
-      Feeling inspired? Customize your <br /> own Dolby.io meeting experience!
+      
+  Thanks for trying aichatforest.com <br /> Ready to try another app type?
     </div>
     <a
       href="https://www.aichatforest.com"
